b460d104cd87a27a6a4d1673086ab5024d4db6a1
Wesley <wesley.firmino@nepreconsulting.com>
2026-08-26 16:54:34 -0300

    feat: start adding tests for validator and nuGet libraries

---
+++ tests/CommomTestUtilities/Requests/RequestRegisterUserAccountJsonBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using MyRecipeBook.communication.Requests;
+
+namespace CommomTestUtilities.Requests
+{
+    public class RequestRegisterUserAccountJsonBuilder
+    {
+        public static RequestRegisterUserAccountJson Build()
+        {
+            return new Faker<RequestRegisterUserAccountJson>()
+                .RuleFor(request => request.Name, f => f.Person.FullName)
+                .RuleFor(request => request.Email, (f, user) => f.Internet.Email(user.Name))
+                .RuleFor(request => request.Password, f => f.Internet.Password())
+                .Generate();
+        }
+    }
+}
---
+++ tests/Valdator.tests/User/Register/RegisterUserAccountUseCaseTests.cs
@@ -0,0 +1,34 @@
+﻿using CommomTestUtilities.Requests;
+using MyRecipeBook.application.UseCases.User.Register;
+using Shouldly;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Validator.tests.User.Register
+{
+    public class RegisterUserAccountUseCaseTests
+    {
+        [Fact]
+        public void Success()
+        {
+            //arrange
+            var request = RequestRegisterUserAccountJsonBuilder.Build();
+            var validator = new RegisterUserAccountValidator();
+
+            //act
+
+            var result = validator.Validate(request);
+
+            //assert
+
+            result.IsValid.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void Validate_shouldHaveError_WhenNameIsEmpty()
+        {
+
+        }
+
+
+    }
+}
